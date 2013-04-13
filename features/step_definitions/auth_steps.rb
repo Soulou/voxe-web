4b02986a8c97c6a8b16db4ddb8130220da938e51
Soulou <leo.unbek@gmail.com>
2013-04-13 12:21:07 +0200

    Update Factory girl API call

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -1,11 +1,15 @@
 # encoding: utf-8
 Given /^I am a logged admin$/ do
-  @current_user = Factory :admin,
+
+  @current_user = FactoryGirl.create :admin,
     email: 'admin@voxe.org',
     password: 'password',
     password_confirmation: 'password'
+
   visit '/users/sign_in'
-  fill_in 'user_email', with: 'admin@voxe.org'
-  fill_in 'user_password' , with: 'password'
+  within("#session") do
+    fill_in 'user_email', with: 'admin@voxe.org'
+    fill_in 'user_password' , with: 'password'
+  end
   click_button 'Sign in'
 end
